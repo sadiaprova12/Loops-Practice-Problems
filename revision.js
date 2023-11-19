e226e770035978be1b21738fc5862643bb3c7f11
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-19 22:28:38 +0600

    added revison js

---
+++ revision.js
@@ -0,0 +1,34 @@
+// String type variable
+var bottle = 'Water';
+var bottleColor = 'Yellow';
+var penColor = 'blue';
+// Number type variable
+var sunglassPrice = 120;
+// Boolean type variable
+var isExpensive = true;
+
+// Array declare
+var tableItems = ['Bottle', 'Pen', 'Mouse', 'Sunglasses', 'Paper'];
+
+// iteam 3 pete chai
+var item3 = tableItems[3];
+// 4 no item name change kore chai
+tableItems[4]= 'NoteBook';
+// kuno ekta jinish jani but sei item ba element r index koto no seta dekhte chai
+var penIndex = tableItems.indexOf('Pen');
+
+// Push [kuno item add korte chai tahole Push dibi]
+// Pop [kuno item ba element remove korte chai tahole Pop dibo]
+
+// Conditional 
+// kivabe decision make kori???
+
+if(tableItems.length > 4){
+
+}
+else if (tableItems[3] == 'Pen'){
+
+}
+else{
+
+}
