dbba3b71cba71d35ddf7d2586dc5c9580c835dc5
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-20 19:35:38 +0600

    added for js

---
+++ for.js
@@ -0,0 +1,33 @@
+// var roastGiven = 0;
+// // while loop
+// while(roastGiven < 7 ){
+//     console.log('please,give me a one pics roast');
+//     roastGiven++;
+// }
+
+
+// for loop
+// for(var roasrGiven = 0; roasrGiven < 7 ; roasrGiven++){
+//     console.log('please,give me a one pics roast');
+// }
+
+// simple version of for loop
+// console.log('simple for loop');
+// for(var i = 0; i < 7; i++){
+//     console.log(i);
+// }
+
+// 0 to 10 numbers
+// for(var i = 0; i <=10; i++){
+//     console.log(i);
+// }
+
+// Even
+// for(var i = 0; i <=10; i+=2){
+//     console.log(i);
+// }
+
+// Odd
+for(var i = 1; i <=10; i+=2){
+    console.log(i);
+}
