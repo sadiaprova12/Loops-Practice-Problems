c0518031691de8e65e1da5d4f542e759656cd33c
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-20 17:59:07 +0600

    added while loop

---
+++ while.js
@@ -0,0 +1,20 @@
+// if(condition){
+
+//}
+
+     // roastGiven = roastGiven +1;
+    // roasrGiven += 1;
+
+
+    /**
+     * 1. Loop variable
+     * 2. Condition inside while
+     * 3. Loop Body
+     * 4. Change the loop variable
+     */
+var roastGiven = 0;
+while(roastGiven < 7){
+    console.log('Please, Give me one pice roast!');
+    console.log(roastGiven);
+    roastGiven++;
+}
