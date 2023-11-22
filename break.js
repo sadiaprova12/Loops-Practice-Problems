872557e8640dcc25d4d3dcf77f8d3ace3149b7f2
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-22 22:06:42 +0600

    added roast given

--- break.js
+++ break.js
@@ -1,6 +1,15 @@
-for(var i=0; i <= 20; i++){
-    console.log(i);
-    if(i > 10){
+// for(var i=0; i <= 20; i++){
+//     console.log(i);
+//     if(i > 10){
+//         break;
+//     }
+// }
+
+var roastGiven = 0;
+while(roastGiven < 10){
+    console.log('Give me a roast!');
+    roastGiven++;
+    if(roastGiven < 4){
         break;
     }
 }
