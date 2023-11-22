5d7f3798ebbc767d05fbb40129ffe194794f6e85
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-22 22:44:32 +0600

    added while lopp way

---
+++ reverse.js
@@ -0,0 +1,13 @@
+// var num = 0;
+// while (num < 10){
+//     console.log(num);
+//     num++;
+// }
+
+// While loop reverse way
+var num = 10;
+while (num >= 1){
+    //console.log(num);
+    num--;
+}
+
