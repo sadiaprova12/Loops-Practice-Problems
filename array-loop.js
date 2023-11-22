a0666a09fa5b55f9fa06ff234c824729a84856dc
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-22 17:54:31 +0600

    added items length

--- array-loop.js
+++ array-loop.js
@@ -3,9 +3,16 @@
 } */
 
 // target: display every elements of an array 
-var numbers = [45, 87, 89, 56, 32, 51, 25];
+// var numbers = [45, 87, 89, 56, 32, 51, 25];
 
-for(var i =0; i < 7; i++){
-    var number = numbers[i];
-    console.log(number);
+// for(var i =0; i < 7; i++){
+//     var number = numbers[i];
+//     console.log(number);
+// }
+
+var items = ['bottle', 'mouse', 'sunglass', 'pen'];
+
+for(var i =0; i<items.length; i++){
+    var item = items[i];
+    console.log(item);
 }
