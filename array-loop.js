e642703e1303f40793116b7565ef6c8ab56dcedc
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-22 17:50:15 +0600

    added display ebery elements

--- array-loop.js
+++ array-loop.js
@@ -1,3 +1,11 @@
-for(var i = 0; i < 10; i++){
+/*for(var i = 0; i < 10; i++){
     console.log(i);
+} */
+
+// target: display every elements of an array 
+var numbers = [45, 87, 89, 56, 32, 51, 25];
+
+for(var i =0; i < 7; i++){
+    var number = numbers[i];
+    console.log(number);
 }
